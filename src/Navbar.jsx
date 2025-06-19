dcec8ad37aa22e57db1be616ca9177321388e9ec
yogeshwaran6414 <yw884422@gmail.com>
2025-06-19 14:24:25 +0530

    Add files via upload

---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='nav-container'>
+        <h1 className='nav-h1'>SmartPick</h1>
+        <ul className='nav-ul'>
+            <li className='nav-li'>SHOP</li>
+            <li className='nav-li'>SKILLS</li>
+            <li className='nav-li'>STORIES</li>
+            <li className='nav-li'>ABOUT US</li>
+            <li className='nav-li'>CONTACT US</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
